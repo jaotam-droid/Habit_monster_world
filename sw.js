76c068ab265b8f3ecb97addec1549a5947d11ff4
jaotam-droid <jaotam@gmail.com>
2026-08-24 10:37:21 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="hmw-v26-1-real3d-fix";
+const CACHE="hmw-v26-2-mobile3d-fix";
 const CORE=["./","./index.html","./manifest.webmanifest"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)));self.skipWaiting();});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
