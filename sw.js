3ec957e4c8f8000f72a14cd89fbe8c85f70f270e
jaotam-droid <jaotam@gmail.com>
2026-08-26 16:39:53 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,12 @@
-const CACHE='hmw-v57-interaction-progress-fix';
+const CACHE='hmw-v58-stable-switch';
 self.addEventListener('install',e=>self.skipWaiting());
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
-self.addEventListener('fetch',e=>{if(e.request.mode==='navigate')e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match(e.request)))});
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys()
+    .then(ks=>Promise.all(ks.map(k=>caches.delete(k))))
+    .then(()=>self.clients.claim()));
+});
+self.addEventListener('fetch',e=>{
+  if(e.request.mode==='navigate'){
+    e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match(e.request)));
+  }
+});
